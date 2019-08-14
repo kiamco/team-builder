e0f2836dbbc7e5cecd5662bdc4900bf80faee80b
kiamco <kiamcoo@gmail.com>
2019-08-14 14:12:54 -0700

    add initial obj value

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 
 function App() {
   const [teamMember, setTeamMember] = useState([{
-    name: '',
-    age: '',
-    id: '',
-    birthday: '',
-    address: '',
-    job: ''
+    name: 'kim',
+    age: '24',
+    id: '20259571',
+    birthday: '11/10/1994',
+    address: '34155 paviea',
+    job: 'analyst'
   }]);
 
   const addNewTeammate = newMember => {
